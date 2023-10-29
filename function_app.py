5ac405a5642784209cedc9e7792e6090fb3d1a1a
Florian Mandlmeier <florian.mandlmeier@nts.eu>
2023-10-29 15:57:54 +0100

    test

--- function_app.py
+++ function_app.py
@@ -7,8 +7,9 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="vivenuautocheckin")
 def vivenuautocheckin(req: func.HttpRequest) -> func.HttpResponse:
-    authorization = os.environ["VIVENU_AUTH"]
+    #authorization = os.environ["VIVENU_AUTH"]
     categoryRef = os.environ["VIVENU_CATEGORYREF"]
+    print(categoryRef)
     try:
         req_body = req.get_json() 
     except ValueError: 
@@ -18,8 +19,8 @@ def vivenuautocheckin(req: func.HttpRequest) -> func.HttpResponse:
         ticket_barcode = req_body['data']['ticket']['barcode']
 
     if ticket_categoryRef == categoryRef: 
-        response = requests.post(f'https://vivenu.com//api/accessusers/tickets/{ticket_barcode}/scan', headers=f'{"Authorization": "{authorization}"}')
-        return func.HttpResponse(response) 
+        #response = requests.post(f'https://vivenu.com//api/accessusers/tickets/{ticket_barcode}/scan', headers=f'{"Authorization": "{authorization}"}')
+        return func.HttpResponse("Abendkasse") 
     else: 
         return func.HttpResponse( 
             "keine Abendkasse", 
