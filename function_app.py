6cb7d4fc777486b8d1c25531c2a59549d63cfe01
Florian Mandlmeier <florian.mandlmeier@nts.eu>
2023-10-31 18:01:27 +0100

    test with pos

--- function_app.py
+++ function_app.py
@@ -8,22 +8,20 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="vivenuautocheckin")
 def vivenuautocheckin(req: func.HttpRequest) -> func.HttpResponse:
     token = os.environ["VIVENU_TOKEN"]
-    categoryRef = os.environ["VIVENU_CATEGORYREF"]
-    print(categoryRef)
     try:
-        req_body = req.get_json() 
+        req_body = req.get_json()
     except ValueError: 
         pass 
     else: 
-        ticket_categoryRef = req_body['data']['ticket']['categoryRef']
-        ticket_barcode = req_body['data']['ticket']['barcode']
-
-    if ticket_categoryRef == categoryRef: 
-        headers = {'token': f'{token}'}
-        response = requests.post(f'https://vivenu.com//api/accessusers/tickets/{ticket_barcode}/scan', headers=headers)
-        return func.HttpResponse(f'StatusCode {response.status_code} and {response.text}')
-    else: 
-        return func.HttpResponse( 
-            "keine Abendkasse", 
-            status_code=200 
-        )
+        try:
+            ticket_barcode = req_body['data']['ticket']['barcode']
+            ticket_origin = req_body['data']['ticket']['origin']
+        except KeyError:
+            pass
+        else:
+            if ticket_origin == "pos": 
+                headers = {'token': f'{token}'}
+                response = requests.post(f'https://vivenu.com//api/accessusers/tickets/{ticket_barcode}/scan', headers=headers)
+                return func.HttpResponse(f'StatusCode {response.status_code} and {response.text}', status_code=response.status_code)
+            else: 
+                return func.HttpResponse("No POS Ticket", status_code=200)
