5abf857e398618d619de1fbd16bf8a8364e9b4fa
Florian Mandlmeier <florian.mandlmeier@nts.eu>
2023-11-01 20:06:57 +0100

    fix double forward-slash and add env variable

--- function_app.py
+++ function_app.py
@@ -8,6 +8,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 @app.route(route="vivenuautocheckin")
 def vivenuautocheckin(req: func.HttpRequest) -> func.HttpResponse:
     token = os.environ["VIVENU_TOKEN"]
+    env = os.environ["VIVENU_ENV"]
     try:
         req_body = req.get_json()
     except ValueError: 
@@ -21,7 +22,7 @@ def vivenuautocheckin(req: func.HttpRequest) -> func.HttpResponse:
         else:
             if ticket_origin == "pos": 
                 headers = {'token': f'{token}'}
-                response = requests.post(f'https://vivenu.com//api/accessusers/tickets/{ticket_barcode}/scan', headers=headers)
+                response = requests.post(f'https://{env}/api/accessusers/tickets/{ticket_barcode}/scan', headers=headers)
                 return func.HttpResponse(f'StatusCode {response.status_code} and {response.text}', status_code=response.status_code)
             else: 
                 return func.HttpResponse("No POS Ticket", status_code=200)
