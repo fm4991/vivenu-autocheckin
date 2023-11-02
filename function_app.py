a37f2e22db20fcdd8ae038dec1fe731900777e38
Florian Mandlmeier <f@mand.li>
2023-11-02 17:10:37 +0100

    prädikat qualitycheck by ass

--- function_app.py
+++ function_app.py
@@ -21,28 +21,26 @@ def vivenuautocheckin(req: func.HttpRequest) -> func.HttpResponse:
     hmac_key = os.environ["VIVENU_HMACKEY"]
     try:
         req_body = req.get_json()
-        req_raw = (req.get_body()).decode()
-        hmac_hash = req.headers['x-vivenu-signature']    
+        req_raw = (req.get_body()).decode()    
     except ValueError: 
-        return func.HttpResponse("Error parsing Webhook Data (JSON)", status_code=400)
+        return func.HttpResponse("Error parsing Webhook Data", status_code=400)
+
+    try:
+        hmac_hash = req.headers['x-vivenu-signature']
+        if verifysignature(req_raw, hmac_hash, hmac_key) is False:
+            return func.HttpResponse(f"401 Unauthorized", status_code=401)
+    except:
+        return func.HttpResponse("401 Unauthorized", status_code=401)
+    
+    try:
+        ticket_barcode = req_body['data']['ticket']['barcode']
+        ticket_origin = req_body['data']['ticket']['origin']
     except KeyError:
-        return func.HttpResponse("Error parsing Webhook Data (HMAC)", status_code=400)
-    else:
-        try:
-            if verifysignature(req_raw, hmac_hash, hmac_key) is False:
-                return func.HttpResponse(f"401 Unauthorized... {type(req_raw)} || {req_body}", status_code=401)
-        except:
-            return func.HttpResponse("401 Unauthorized!", status_code=401)
-        else:
-            try:
-                ticket_barcode = req_body['data']['ticket']['barcode']
-                ticket_origin = req_body['data']['ticket']['origin']
-            except KeyError:
-                return func.HttpResponse("Expected Keys not found in Webhook Data, not a POS Ticket", status_code=200)
-            else:
-                if ticket_origin == "pos": 
-                    headers = {'token': f'{token}'}
-                    response = requests.post(f'https://{env}/api/accessusers/tickets/{ticket_barcode}/scan', headers=headers)
-                    return func.HttpResponse(f'StatusCode {response.status_code} and {response.text}', status_code=response.status_code)
-                else: 
-                    return func.HttpResponse("No POS Ticket", status_code=200)
+        return func.HttpResponse("Expected Keys not found in Webhook Data, not a POS Ticket", status_code=200)
+
+    if ticket_origin == "pos": 
+        headers = {'token': f'{token}'}
+        response = requests.post(f'https://{env}/api/accessusers/tickets/{ticket_barcode}/scan', headers=headers)
+        return func.HttpResponse(f'StatusCode {response.status_code} and {response.text}', status_code=response.status_code)
+    else: 
+        return func.HttpResponse("No POS Ticket", status_code=200)
